eb2fc4c9d74b537492936df3b2cc169767759e9d
Thomas DRS <toderesa97@gmail.com>
2017-02-20 17:55:37 +0000

    Add files via upload

--- string.c
+++ string.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 char* mistr_copy(char* s1, char* s2);
+char* mistr_copy(char* s1, char* s2);
 int str_len(char* c);
+char* mi_strcat(char* s1, char* s2);
 
 int main(){
+
+	
+	char s1[100] = "Hi you there!";
+	char s2[100] = ", how are you?";
+	mi_strcat(s1, s2);
+
+	//expected: Hi you there!, how are you?
+	printf("expected:%s\n", s1);
 	
 }
 
@@ -22,4 +32,14 @@ char *mistr_copy(char *s1, char *s2){
 	}
 	*ptrs1 = final;
 	return ptrs1;
+}
+
+char* mi_strcat(char* s1, char* s2){
+	while(*s1){
+		s1++;
+	}
+	while(*s2){
+		*(s1++) = *(s2++);
+	}
+	*s1 = '\0';
 }
