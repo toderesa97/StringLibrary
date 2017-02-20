d131fb391a4f7ce41f04637117a8884f44fa16d0
Thomas DRS <toderesa97@gmail.com>
2017-02-20 17:10:26 +0000

    Add files via upload

---
+++ string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+char* mistr_copy(char* s1, char* s2);
+
+int main(){
+	
+}
+
+int str_len(char *c){
+	int length = 0;
+	char* ptr = c;
+	while(*(ptr++) != 0) length++;
+	return length;	
+}
+
+char *mistr_copy(char *s1, char *s2){
+	char* ptrs2 = s2;
+	char* ptrs1 = s1;
+	char final = '\0';
+	while(*ptrs2 !=0 || *ptrs1 !=0){
+		*(ptrs1++) = *(ptrs2++);
+	}
+	*ptrs1 = final;
+	return ptrs1;
+}
